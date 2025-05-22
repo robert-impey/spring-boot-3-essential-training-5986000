12e24473cfeaa71335dac525bb97455ca49a0864
Robert Impey <robert-impey@users.noreply.github.com>
2025-05-22 13:35:13 +0100

    Created the booting app

---
+++ booting/src/test/java/com/robertimpey/booting/BootingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertimpey.booting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
