7be818b508f18d234d37de86e24cb5bbe9230d84
Robert Impey <robert-impey@users.noreply.github.com>
2025-06-16 14:14:40 +0100

    Init of web app

---
+++ room-web-app/src/main/java/com/robertimpey/room_web_app/RoomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.robertimpey.room_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppApplication.class, args);
+	}
+
+}
