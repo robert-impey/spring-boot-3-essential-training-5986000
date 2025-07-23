1016c09769dcbc66bbe6ce5994953086e6b97f35
Robert Impey <robert-impey@users.noreply.github.com>
2025-07-23 11:02:46 +0100

    Calling the web service from the CLI

---
+++ room-clr/src/main/java/com/frankmoley/lil/roomclr/Room.java
@@ -0,0 +1,12 @@
+package com.frankmoley.lil.roomclr;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class Room {
+    private UUID id;
+    private String name;
+    private String number;
+    private String info;
+}
--- room-clr/src/main/java/com/frankmoley/lil/roomclr/RoomClrApplication.java
+++ room-clr/src/main/java/com/frankmoley/lil/roomclr/RoomClrApplication.java
@@ -3,7 +3,14 @@ package com.frankmoley.lil.roomclr;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RoomClrApplication {
@@ -13,11 +20,17 @@ public class RoomClrApplication {
   }
 
   @Bean
-  public CommandLineRunner run() {
+  RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+  @Bean
+  public CommandLineRunner run(RestTemplate template) {
     return args -> {
-      for(int i=1; i<=100;i++){
-        System.out.println("Counting: " + i);
-      }
+      ResponseEntity<List<Room>> rooms = template.exchange("http://localhost:8080/api/rooms",
+              HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {});
+
+      rooms.getBody().forEach(System.out::println);
     };
   }
 
