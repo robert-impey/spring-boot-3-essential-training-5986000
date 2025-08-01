654929fc6d4aa553fa0e63af8ce588ed11ab403e
Robert Impey <robert-impey@users.noreply.github.com>
2025-08-01 15:50:05 +0100

    Form based authentication

---
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.frankmoley.lil.roomwebapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(@NonNull ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index");
+        registry.addViewController("/login").setViewName("/login");
+    }
+}
