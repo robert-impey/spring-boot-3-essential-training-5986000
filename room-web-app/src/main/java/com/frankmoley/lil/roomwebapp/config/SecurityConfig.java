1e3ceb85b4b98db8137c2d4160e7999840fa7bc4
Robert Impey <robert-impey@users.noreply.github.com>
2025-08-31 21:43:16 +0100

    Adding the rest repository starter

--- room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/config/SecurityConfig.java
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests((requests)-> requests
-            .requestMatchers("/", "/api/*").permitAll()
+            .requestMatchers("/", "/api/*", "api/rest/*").permitAll()
             .anyRequest().authenticated()
         )
         .formLogin((form) -> form
